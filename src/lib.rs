57da35ce5c3aa0fa6e6ab020d50271c846c97db6
sozysozbot <sozysozbot@gmail.com>
2022-12-19 17:56:59 +0900

    ver 0.2.5: Update `Coord`'s size and add `PureMove`

--- src/lib.rs
+++ src/lib.rs
@@ -209,16 +209,16 @@ impl Field {
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 pub struct Coord {
-    row_index: usize,
-    col_index: usize,
+    row_index: u8,
+    col_index: u8,
 }
 
 impl Coord {
     pub const fn new(row_index: usize, col_index: usize) -> Option<Self> {
         if row_index < 9 && col_index < 9 {
             Some(Coord {
-                row_index,
-                col_index,
+                row_index: row_index as u8,
+                col_index: col_index as u8,
             })
         } else {
             None
@@ -233,13 +233,10 @@ impl Coord {
         }
         //  `?` is not allowed in a `const fn`
         match (
-            add(self.row_index, row_delta),
-            add(self.col_index, col_delta),
+            add(self.row_index as usize, row_delta),
+            add(self.col_index as usize, col_delta),
         ) {
-            (Some(row_index), Some(col_index)) => Some(Coord {
-                row_index,
-                col_index,
-            }),
+            (Some(row_index), Some(col_index)) => Coord::new(row_index, col_index),
             _ => None,
         }
     }
@@ -372,15 +369,15 @@ impl Board {
         })
     }
     pub fn peek(&self, c: Coord) -> Option<PieceWithSide> {
-        self.0[c.row_index][c.col_index]
+        self.0[c.row_index as usize][c.col_index as usize]
     }
     pub fn pop(&mut self, c: Coord) -> Option<PieceWithSide> {
-        let p = self.0[c.row_index][c.col_index];
+        let p = self.0[c.row_index as usize][c.col_index as usize];
         self.put(c, None);
         p
     }
     pub fn put(&mut self, c: Coord, p: Option<PieceWithSide>) {
-        self.0[c.row_index][c.col_index] = p
+        self.0[c.row_index as usize][c.col_index as usize] = p
     }
 
     pub fn assert_empty(&self, c: Coord) {
@@ -782,4 +779,88 @@ mod tests {
     fn size() {
         assert_eq!(std::mem::size_of::<Field>(), 93);
     }
+}
+
+/// Describes a move denoted in absolute coordinates.
+/// ／絶対座標で書かれた指し手を表す。
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub enum PureMove {
+    /// A non-Tam2 piece moves from a square on a board to another square without stepping.
+    /// ／皇ではない駒が、盤上から盤上に踏越えなしで移動する。
+    NonTamMoveSrcDst {
+        /// origin／開始点
+        src: Coord,
+        /// final destination／終了点
+        dest: Coord,
+        /// whether a water-entry ciurl is required／入水判定が必要かどうか
+        is_water_entry_ciurl: bool,
+    },
+    /// A non-Tam2 piece moves from a square on a board to another square, during which it steps another piece and does a finite movement.
+    /// ／皇ではない駒が、盤上から盤上に踏越えを伴いながら移動し、踏越え後は有限移動をする。
+    NonTamMoveSrcStepDstFinite {
+        /// origin／開始点
+        src: Coord,
+        /// via point／経由点
+        step: Coord,
+        /// destination／終了点
+        dest: Coord,
+        /// whether a water-entry ciurl is required／入水判定が必要かどうか
+        is_water_entry_ciurl: bool,
+    },
+    /// A non-Tam2 piece moves from a square on a board to another square, during which it steps another piece and tries to do a directional, infinite movement.
+    /// ／皇ではない駒が、盤上から盤上に踏越えを伴いながら移動し、踏越え後は無限移動をしようとする。
+    InfAfterStep {
+        /// origin／開始点
+        src: Coord,
+        /// via point／経由点
+        step: Coord,
+        /// the planned LOCATION. After casting the sticks, some rules necessitates that you go to the destination or to the direction that you have declared beforehand.
+        /// Hence the confusing name.
+        /// ／行く予定の場所。踏越え判定後の移動先は、ルールによっては「計画したマス」である必要があったり「計画した方角」である必要があったりする。
+        planned_direction: Coord,
+    },
+    /// A non-Tam2 piece moves from hop1zuo1 to a square on a board.
+    /// ／皇ではない駒が、手駒から盤上に移動する。
+    NonTamMoveFromHopZuo {
+        /// color／駒の色
+        color: Color,
+        /// profession／駒の役職
+        prof: Profession,
+        /// destination／終了点
+        dest: Coord,
+    },
+    /// A Tam2 moves from a square on a board to another square without stepping.
+    /// ／皇が盤上から盤上に踏越えなしで移動する。
+    TamMoveNoStep {
+        /// origin／開始点
+        src: Coord,
+        /// first destination／一回目の終了点
+        first_dest: Coord,
+        /// second destination／二回目の終了点
+        second_dest: Coord,
+    },
+    /// A Tam2 moves from a square on a board to another square. In the former half of its movement, it steps another piece.
+    /// ／皇が盤上から盤上に移動し、一回目の移動の最中に踏越えをする。
+    TamMoveStepsDuringFormer {
+        /// origin／開始点
+        src: Coord,
+        /// via point／経由点
+        step: Coord,
+        /// first destination／一回目の終了点
+        first_dest: Coord,
+        /// second destination／二回目の終了点
+        second_dest: Coord,
+    },
+    /// A Tam2 moves from a square on a board to another square. In the latter half of its movement, it steps another piece.
+    /// ／皇が盤上から盤上に移動し、二回目の移動の最中に踏越えをする。
+    TamMoveStepsDuringLatter {
+        /// origin／開始点
+        src: Coord,
+        /// via point／経由点
+        step: Coord,
+        /// first destination／一回目の終了点
+        first_dest: Coord,
+        /// second destination／二回目の終了点
+        second_dest: Coord,
+    },
 }
